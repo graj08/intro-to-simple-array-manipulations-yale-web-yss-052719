2a20f5f746b5af2e1f3a3963b2500ece581a9175
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 11:25:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,23 @@ end
 def using_concat (arg1,arg2)
   arg1.concat(arg2)
 end
+
+def using_insert (array,element)
+  array.insert(4,element)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array,string)
+  array.delete(string)
+end
+
+def using_delete_at (array,int)
+  array.delete_at(int)
+end
